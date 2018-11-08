754260bbf45393fd484bff002823ae124e91c7fa
コンドウ・マサト <masato.kondo@gmail.com>
2018-11-08 21:21:35 +0900

    Initial Commit

---
+++ SpeedAndDirection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedAndDirection
+//
+//  Created by MK on 2018/11/08.
+//  Copyright © 2018 NotenkiApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
